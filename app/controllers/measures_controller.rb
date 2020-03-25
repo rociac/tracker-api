337416957bcf0e5ae60b589fbccc682b692be05a
Rodolfo <brsrkdev@gmail.com>
2020-03-25 15:18:47 -0600

    Add measures controller, Fix tests

--- app/controllers/measures_controller.rb
+++ app/controllers/measures_controller.rb
@@ -1,2 +1,37 @@
 class MeasuresController < ApplicationController
+  before_action :set_measure, only: [:show, :update, :destroy]
+
+  def index
+    @measures = Measure.all
+    json_response(@measures)
+  end
+
+  def create
+    @measure = Measure.create!(measure_params)
+    json_response(@measure, :created)
+  end
+
+  def show
+    json_response(@measure)
+  end
+
+  def update
+    @measure.update(measure_params)
+    head :no_content
+  end
+
+  def destroy
+    @measure.destroy
+    head :no_content
+  end
+
+  private
+
+  def measure_params
+    params.permit(:name)
+  end
+
+  def set_measure
+    @measure = Measure.find(params[:id])
+  end
 end
